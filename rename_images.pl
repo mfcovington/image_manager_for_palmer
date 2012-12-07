518d8a62df5455c87c6e05d4b9f6ccefca4cd551
Mike Covington <mfcovington@gmail.com>
2012-12-06 23:56:53 -0800

    sub rename_images

--- rename_images.pl
+++ rename_images.pl
@@ -25,26 +25,33 @@ my $options = GetOptions(
 my $autotransfer_dir = $img_dir . "/auto_transfer/";
 my $organized_dir    = $img_dir . "/organized/";
 
+
 my @images;
 find( sub { push @images, $File::Find::name if /IMG_\d*\.CR2$/ },
     $autotransfer_dir );
 
-for my $image_name (@images) {
-    my $info_subset =
-      ImageInfo( $image_name, 'OwnerName', 'CreateDate', 'WB_RGGBLevels',
-        'ColorTempAsShot', 'FileType', 'Directory' );
-    my $camera_name = $info_subset->{'OwnerName'};
-    my ( $date, $time ) = split / /, $info_subset->{'CreateDate'};
-    $date =~ s/://g;
-    $time =~ s/://g;
-    my $format = $info_subset->{'FileType'};
-    my $image_name_new = join ".", $date, $time, $camera_name, $format;
-    my $out_dir = $info_subset->{'Directory'};
-    move( $image_name, $organized_dir . $image_name_new );
+rename_images(@images);
+
+sub rename_images {
+    for my $image_name (@_) {
+        my $info_subset = ImageInfo(
+            $image_name, 'OwnerName', 'CreateDate', 'MeasuredEV', 'FileType'
+        );
+        my $camera_name = $info_subset->{'OwnerName'};
+        my ( $date, $time ) = split / /, $info_subset->{'CreateDate'};
+        $date =~ s/://g;
+        $time =~ s/://g;
+        my $format = $info_subset->{'FileType'};
+        my $image_name_new = join ".", $date, $time, $camera_name, $format;
+        my $out_dir = $organized_dir;
+        move( $image_name, $out_dir . $image_name_new );
+    }
 }
 
 exit;
 
 __END__
 
+make dirs if needed
+move darktime images to different dir
 Make log of all transfers and renaming!
